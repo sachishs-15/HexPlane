0257a3a02c435b1685db4a8d04db01277748b6bb
sachishs-15 <59312090+sachishs-15@users.noreply.github.com>
2024-02-10 13:24:34 +0530

    Update __init__.py

--- hexplane/dataloader/__init__.py
+++ hexplane/dataloader/__init__.py
@@ -1,5 +1,6 @@
 from .dnerf_dataset import DNerfDataset
 from .neural_3D_dataset_NDC import Neural3D_NDC_Dataset
+from .phototourism import Phototourism
 
 
 def get_train_dataset(cfg, is_stack=False):
@@ -32,6 +33,19 @@ def get_train_dataset(cfg, is_stack=False):
             eval_step=cfg.data.nv3d_ndc_eval_step,
             eval_index=cfg.data.nv3d_ndc_eval_index,
             sphere_scale=cfg.data.nv3d_ndc_sphere_scale,
+        )
+    elif cfg.data.dataset_name == "phototourism":
+        train_dataset = Phototourism(
+            cfg.data.datadir,
+            "train",
+            cfg.data.downsample,
+            is_stack=is_stack,
+            cal_fine_bbox=cfg.data.cal_fine_bbox,
+            N_vis=cfg.data.N_vis,
+            time_scale=cfg.data.time_scale,
+            scene_bbox_min=cfg.data.scene_bbox_min,
+            scene_bbox_max=cfg.data.scene_bbox_max,
+            N_random_pose=cfg.data.N_random_pose,
         )
     else:
         raise NotImplementedError("No such dataset")
@@ -68,6 +82,19 @@ def get_test_dataset(cfg, is_stack=True):
             eval_step=cfg.data.nv3d_ndc_eval_step,
             eval_index=cfg.data.nv3d_ndc_eval_index,
             sphere_scale=cfg.data.nv3d_ndc_sphere_scale,
+        )
+    elif cfg.data.dataset_name == "phototourism":
+        test_dataset = Phototourism(
+            cfg.data.datadir,
+            "test",
+            cfg.data.downsample,
+            is_stack=is_stack,
+            cal_fine_bbox=cfg.data.cal_fine_bbox,
+            N_vis=cfg.data.N_vis,
+            time_scale=cfg.data.time_scale,
+            scene_bbox_min=cfg.data.scene_bbox_min,
+            scene_bbox_max=cfg.data.scene_bbox_max,
+            N_random_pose=cfg.data.N_random_pose,
         )
     else:
         raise NotImplementedError("No such dataset")
